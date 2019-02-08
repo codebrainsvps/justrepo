e88b8034cd20536b222c1806f5f26c5b5f36d055
aditya <aditya.samena@gmail.com>
2019-02-08 19:42:37 +0530

    max java file

---
+++ max.java
@@ -0,0 +1,14 @@
+public class max
+{
+public static void main(String args[])
+{
+int a=100,b=20;
+System.out.println("value of a"+a);
+System.out.println("value of b"+b);
+System.out.println();
+if(a>b)
+System.out.println("maximum is"+a);
+else
+System.out.println("maximum is"+b);
+}
+}
